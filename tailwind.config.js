0c8641d0696b2e1ecb2c9cf1a381e8a8edc7f96a
Jakar Ahmed <jakarahmed970@gmail.com>
2024-01-21 19:23:40 +0600

    dasy ui repo

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+
+
+        screens: {
+            sm: '240px',
+            lg:'600px',
+      
+           
+          },
+
+      extend: {},
+    },
+    plugins: [],
+  }
